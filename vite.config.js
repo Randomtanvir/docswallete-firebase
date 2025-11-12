198dba5cd9ee34a6a6a23331c7e36099c883be7a
Tanvir <tanvir.info07@gmail.com>
2025-11-12 23:41:11 +0600

    base path change

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "./", // ⚠️ SPA deploy এর জন্য
+  base: "/", // ⚠️ SPA deploy এর জন্য
 });
